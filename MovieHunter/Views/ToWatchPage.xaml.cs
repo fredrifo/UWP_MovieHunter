b6317c44f9bc411f469fb804dac8cc7fe16f2e6b
fredrifo <fredrik.forsell@lyse.net>
2019-04-13 17:00:30 -0700

    ListView implementation of object

--- MovieHunter/Views/ToWatchPage.xaml.cs
+++ MovieHunter/Views/ToWatchPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.ObjectModel;
 using MovieHunter.ViewModels;
 
 using Windows.UI.Xaml.Controls;
@@ -8,6 +8,8 @@ namespace MovieHunter.Views
 {
     public sealed partial class ToWatchPage : Page
     {
+        private ObservableCollection<MainPage> movies;
+
         private ToWatchViewModel ViewModel
         {
             get { return ViewModelLocator.Current.ToWatchViewModel; }
@@ -16,6 +18,14 @@ namespace MovieHunter.Views
         public ToWatchPage()
         {
             InitializeComponent();
+
+            movies = new ObservableCollection<MainPage>()
+            {
+                new MainPage(){CoverTitle = "Forrest Gump"},
+                new MainPage(){CoverTitle = "Pirates of the"}
+            };
+
+            list.ItemsSource = movies;
         }
     }
 }
