edf5d55fee3112e04c8914a13edb22cfaf11b10c
fredrifo <fredrik.forsell@lyse.net>
2019-04-14 20:33:01 -0700

    clickevent for horizontal listView

--- MovieHunter/Views/ToWatchPage.xaml.cs
+++ MovieHunter/Views/ToWatchPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using MovieHunter.ViewModels;
-
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace MovieHunter.Views
@@ -19,13 +19,56 @@ namespace MovieHunter.Views
         {
             InitializeComponent();
 
+            string coverurl = "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Forrest_Gump_poster.jpg/220px-Forrest_Gump_poster.jpg";
+
             movies = new ObservableCollection<MainPage>()
             {
-                new MainPage(){CoverTitle = "Forrest Gump"},
-                new MainPage(){CoverTitle = "Pirates of the"}
+                new MainPage(){CoverTitle = "Forrest Gump", CoverUri = coverurl},
+                new MainPage(){CoverTitle = "Pirates of the", CoverUri = coverurl},
+
+
+                new MainPage("This one", "Drama", "1", "2", "3", coverurl),
+
+
+                new MainPage(){CoverTitle = "Pirates of the", CoverUri = coverurl},
+                new MainPage(){CoverTitle = "Forrest Gump", CoverUri = coverurl},
+                new MainPage(){CoverTitle = "Pirates of the", CoverUri = coverurl},
+                new MainPage(){CoverTitle = "Forrest Gump", CoverUri = coverurl},
+                new MainPage(){CoverTitle = "Pirates of the", CoverUri = coverurl},
+                new MainPage(){CoverTitle = "Forrest Gump", CoverUri = coverurl},
+                new MainPage(){CoverTitle = "Pirates of the", CoverUri = coverurl},
             };
 
             list.ItemsSource = movies;
         }
+
+        private async void openMovieAsync(object sender, ItemClickEventArgs e)
+        {
+            MainPage tappeditem = e.ClickedItem as MainPage;
+
+            if (e.ClickedItem != null)
+            {
+                //saving a copy of the selected list object:
+                tappeditem = e.ClickedItem as MainPage;
+
+                // Window.Current.Content = new MainPage(tappeditem.CoverTitle, "Category", "string Director", "string Stars", tappeditem.Summary, tappeditem.CoverUri);
+                var parameters = tappeditem;
+                Frame.Navigate(typeof(MainPage), parameters);
+
+                ContentDialog noWifiDialog = new ContentDialog
+                {
+                    Title = tappeditem.CoverTitle,
+                    Content = tappeditem.Summary,
+                    CloseButtonText = "Ok"
+                };
+
+                ContentDialogResult result = await noWifiDialog.ShowAsync();
+            }
+
+            
+            //
+            
+        }
+
     }
 }
