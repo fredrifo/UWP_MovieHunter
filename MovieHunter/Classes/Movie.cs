65cd545c892e87509efb419fecce89f313a5bb6d
fredrifo <fredrik.forsell@lyse.net>
2019-04-18 06:51:27 +0200

    Class for creating movie objects

---
+++ MovieHunter/Classes/Movie.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieHunter.Classes
+{
+    public sealed partial class Movie
+    {
+        //Setters and Getters for class
+
+        public string ImageSource
+        {
+            get;
+            set;
+        }
+        public string CoverTitle
+        {
+            get;
+            set;
+        }
+        public string Category
+        {
+            get;
+            set;
+        }
+        public string Director
+        {
+            get;
+            set;
+        }
+        public string Writer
+        {
+            get;
+            set;
+        }
+        public string Stars
+        {
+            get;
+            set;
+        }
+        public string Summary
+        {
+            get;
+            set;
+        }
+        public string CoverUri
+        {
+            get;
+            set;
+        }
+
+
+        //Setter for creating new movie object
+        public Movie(string CoverTitle, string Category, string Director, string Stars, string Summary, string CoverUri)
+        {
+            this.CoverTitle = CoverTitle;
+            this.Category = Category;
+            this.Director = Director;
+            this.Stars = Stars;
+            this.Summary = Summary;
+            this.CoverUri = CoverUri;
+        }
+    }
+}
+    
