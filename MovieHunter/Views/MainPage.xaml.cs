fb624d783217c9c342bd30569ad721f1ab802fc4
fredrifo <fredrik.forsell@lyse.net>
2019-05-04 15:18:58 +0200

    ManPage content was moved to MoviePage

--- MovieHunter/Views/MainPage.xaml.cs
+++ MovieHunter/Views/MainPage.xaml.cs
@@ -1,75 +1,16 @@
 ﻿using System;
 using MovieHunter.Classes;
 using MovieHunter.ViewModels;
-
+using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace MovieHunter.Views
 {
     public sealed partial class MainPage : Page
     {
-        public string ImageSource {
-            get;
-            set;
-        }
-        public string CoverTitle
-        {
-            get;
-            set;
-        }
-        public string Category
-        {
-            get;
-            set;
-        }
-
-        public string Rating
-        {
-            get;
-            set;
-        }
-
-        public string ReleaseYear
-        {
-            get;
-            set;
-        }
-
-        public string Runtime
-        {
-            get;
-            set;
-        }
-
-        public string Director
-        {
-            get;
-            set;
-        }
-        public string Writer
-        {
-            get;
-            set;
-        }
-        public string Stars
-        {
-            get;
-            set;
-        }
-        public string Summary
-        {
-            get;
-            set;
-        }
-        public string CoverUri
-        {
-            get;
-            set;
-        }
-
-
-
         private MainViewModel ViewModel
         {
             get { return ViewModelLocator.Current.MainViewModel; }
@@ -78,51 +19,43 @@ namespace MovieHunter.Views
         public MainPage()
         {
             InitializeComponent();
-            string coverurl = "https://pdfimages.wondershare.com/forms-templates/medium/movie-poster-template-3.png";
 
-            //If page opens with no movie params
-            this.CoverTitle = "Null";
-            this.Category = "Null";
-            this.Director = "Null";
-            this.Writer = "Null";
-            this.Stars = "Null";
-            this.Summary = "Summary: Null";
-            this.CoverUri = coverurl;
-
-            //MainPage movie = new MainPage("Null", "Null", "Null", "Null", "Null", coverurl);
+            CreateRandomGrid();
         }
-
-        public MainPage(string CoverTitle,  string Category, string Director, string Stars, string Summary, string CoverUri, int Rating)
+        public void CreateRandomGrid()
         {
-            InitializeComponent();
+            Grid content = contentGrid;
 
-            //this.CoverTitle = CoverTitle;
-            //this.Category = Category;
-            //this.Director = Director;
-            //this.Stars = Stars;
-            //this.Summary = Summary;
-            //this.CoverUri = CoverUri;
+            Random random = new Random();
 
-        }
+            int columnAmount = 10;
+            int rowAmount = 5;
 
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
-        {
-            var parameters = e.Parameter as Movie;
 
-            if(parameters != null)
+            for (int x = 0; x<rowAmount; x++)
             {
-                this.CoverTitle = parameters.CoverTitle;
-                this.Category = parameters.Category;
-                this.Director = parameters.Director;
-                this.Writer = parameters.Writer;
-                this.Stars = parameters.Stars;
-                this.Summary = parameters.Summary;
-                this.CoverUri = parameters.CoverUri;
-                this.starRating.Width = (6 * 68) / 2; //(6 IMDB score * 68pixels each star) / 2 to get it to a  3/5
-            }
-                
-                
-        }
+                RowDefinition rw = new RowDefinition();
+                rw.Height = new GridLength(1, GridUnitType.Star);
+                content.RowDefinitions.Add(rw);
 
+                for (int i = 0; i<columnAmount; i++)
+                {
+                    
+
+                    ColumnDefinition clm = new ColumnDefinition();
+                    clm.Width = new GridLength(1, GridUnitType.Star);
+
+                    content.ColumnDefinitions.Add(clm);
+
+                    Frame btn = new Frame();
+                    btn.Background = new SolidColorBrush(Color.FromArgb(255, 48, 179, 221));
+                    btn.Margin = new Windows.UI.Xaml.Thickness(2);
+                    btn.SetValue(Grid.RowProperty, x);
+                    btn.SetValue(Grid.ColumnProperty, i);
+                    content.Children.Add(btn);
+                }
+
+            }
+        }
     }
 }
---
+++ MovieHunter/Views/MoviePage.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using MovieHunter.Classes;
+using MovieHunter.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace MovieHunter.Views
+{
+    public sealed partial class MoviePage : Page
+    {
+        public string ImageSource
+        {
+            get;
+            set;
+        }
+        public string CoverTitle
+        {
+            get;
+            set;
+        }
+        public string Category
+        {
+            get;
+            set;
+        }
+
+        public string Rating
+        {
+            get;
+            set;
+        }
+
+        public string ReleaseYear
+        {
+            get;
+            set;
+        }
+
+        public string Runtime
+        {
+            get;
+            set;
+        }
+
+        public string Director
+        {
+            get;
+            set;
+        }
+        public string Writer
+        {
+            get;
+            set;
+        }
+        public string Stars
+        {
+            get;
+            set;
+        }
+        public string Summary
+        {
+            get;
+            set;
+        }
+        public string CoverUri
+        {
+            get;
+            set;
+        }
+
+        private MovieViewModel ViewModel
+        {
+            get { return ViewModelLocator.Current.MovieViewModel; }
+        }
+
+        public MoviePage()
+        {
+            InitializeComponent();
+
+            string coverurl = "https://pdfimages.wondershare.com/forms-templates/medium/movie-poster-template-3.png";
+
+            //If page opens with no movie params
+            this.CoverTitle = "Null";
+            this.Category = "Null";
+            this.Director = "Null";
+            this.Writer = "Null";
+            this.Stars = "Null";
+            this.Summary = "Summary: Null";
+            this.CoverUri = coverurl;
+
+            //MainPage movie = new MainPage("Null", "Null", "Null", "Null", "Null", coverurl);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var parameters = e.Parameter as Movie;
+
+            if (parameters != null)
+            {
+                this.CoverTitle = parameters.CoverTitle;
+                this.Category = parameters.Category;
+                this.Director = parameters.Director;
+                this.Writer = parameters.Writer;
+                this.Stars = parameters.Stars;
+                this.Summary = parameters.Summary;
+                this.CoverUri = parameters.CoverUri;
+                this.starRating.Width = (6 * 68) / 2; //(6 IMDB score * 68pixels each star) / 2 to get it to a  3/5
+            }
+
+
+        }
+
+        private void OpenTrailerPopupClicked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // open the Popup if it isn't open already 
+            if (!TrailerPopup.IsOpen) {
+                TrailerPopup.IsOpen = true;
+            }
+            var c = Window.Current.Bounds;
+            webView2.Width = c.Width - 400;
+            webView2.Height = c.Height-50;
+
+            string videoID = "tYrND5hMY3A";
+            string html = @"<iframe width=""100%"" height=""100%"" src=""http://www.youtube.com/embed/" + videoID + @"?rel=0"" frameborder=""0""></iframe>";
+            webView2.NavigateToString(html);
+        }
+
+        private void CloseTrailerPopupClicked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (TrailerPopup.IsOpen) {
+                TrailerPopup.IsOpen = false;
+            }
+        }
+    }
+}
