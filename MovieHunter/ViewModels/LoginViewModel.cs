90fa0db7aa140242cb71d5e567cf7ef3beaab206
fredrifo <fredrik.forsell@lyse.net>
2019-05-04 15:14:23 +0200

    Added viewmodel

---
+++ MovieHunter/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using GalaSoft.MvvmLight;
+
+namespace MovieHunter.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public LoginViewModel()
+        {
+        }
+    }
+}
