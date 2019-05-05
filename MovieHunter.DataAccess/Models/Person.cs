fcda2d9ac4b5be53ca05e1473d982fb514efe444
fredrifo <fredrik.forsell@lyse.net>
2019-05-05 04:44:11 +0200

    Created a dataAccess library

---
+++ MovieHunter.DataAccess/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieHunter.DataAccess.Models
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            MovieDirector = new HashSet<Movie>();
+            MovieStarNavigation = new HashSet<Movie>();
+            MovieWriter = new HashSet<Movie>();
+        }
+
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Picture { get; set; }
+
+        public ICollection<Movie> MovieDirector { get; set; }
+        public ICollection<Movie> MovieStarNavigation { get; set; }
+        public ICollection<Movie> MovieWriter { get; set; }
+    }
+}
