3a124d035485b3fccea07bcfa2be9149770cad19
Fredrik Forsell <fredrik.forsell@outlook.com>
2019-05-20 00:46:03 +0200

    added viewModel

---
+++ MovieHunter/ViewModels/AdministrationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using GalaSoft.MvvmLight;
+
+namespace MovieHunter.ViewModels
+{
+    public class AdministrationViewModel : ViewModelBase
+    {
+        public AdministrationViewModel()
+        {
+        }
+    }
+}
---
+++ MovieHunter/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using GalaSoft.MvvmLight;
+
+namespace MovieHunter.ViewModels
+{
+    public class RegisterViewModel : ViewModelBase
+    {
+        public RegisterViewModel()
+        {
+        }
+    }
+}
