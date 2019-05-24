70e4c408379c422de768aaa674383036d25ab1c3
Fredrik Forsell <fredrik.forsell@outlook.com>
2019-05-24 21:40:36 +0200

    + new class for Shared controller methods

---
+++ MovieHunter.RESTApi/Controllers/SharedControllerFuntions.cs
@@ -0,0 +1,39 @@
+﻿using MovieHunter.DataAccessCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieHunter.RESTApi.Controllers
+{
+    public class SharedControllerFuntions
+    {
+
+
+        public static Nullable<int> TokenVerificator(string token)
+        {
+            fredrifoContext _context = new fredrifoContext();
+
+            //TODO: check if the token is valid  var list = _context.TokenValidator.Where(d => d.Token == token && d.ValidFrom,...);
+            var list = _context.TokenValidator.Where(d => d.Token == token);
+            User tokenOwner = new User();
+
+            if (list == null)
+            {
+                //The token does not excist in the database
+                return null;
+            }
+
+            //The list should contain 1 item. if it contains multiple items it will use the last one.
+            foreach (TokenValidator u in list)
+            {
+                tokenOwner.UserId = u.UserId;
+            }
+
+            
+
+            //The token excists. 
+            return tokenOwner.UserId;
+        }
+    }
+}
