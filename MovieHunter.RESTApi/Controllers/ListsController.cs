ce0224aa944de4eb868f442c24f3744b97361f50
Fredrik Forsell <fredrik.forsell@outlook.com>
2019-05-24 21:39:57 +0200

    + Controller for Lists

---
+++ MovieHunter.RESTApi/Controllers/ListsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieHunter.DataAccessCore.Models;
+
+namespace MovieHunter.RESTApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListsController : ControllerBase
+    {
+        private readonly fredrifoContext _context;
+
+        public ListsController(fredrifoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Lists
+        [HttpGet]
+        public IEnumerable<List> GetList()
+        {
+            return _context.List;
+        }
+
+        // GET: api/Lists/userLists
+        [HttpPost]
+        [Route("userLists")]
+        public async Task<IActionResult> GetList([FromBody] string token)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Nullable<int> id = SharedControllerFuntions.TokenVerificator(token);
+
+            //ID only excists if it gets an int as the return value
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var list = _context.List.Where(c => c.UserId == id);
+
+            if (list == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(list);
+        }
+
+        // PUT: api/Lists/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutList([FromRoute] int id, [FromBody] List list)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != list.ListId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(list).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Lists/userLists
+        [HttpPost]
+        public async Task<IActionResult> PostList([FromBody] List list)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.List.Add(list);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetList", new { id = list.ListId }, list);
+        }
+
+        // DELETE: api/Lists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteList([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var list = await _context.List.FindAsync(id);
+            if (list == null)
+            {
+                return NotFound();
+            }
+
+            _context.List.Remove(list);
+            await _context.SaveChangesAsync();
+
+            return Ok(list);
+        }
+
+        private bool ListExists(int id)
+        {
+            return _context.List.Any(e => e.ListId == id);
+        }
+    }
+}
