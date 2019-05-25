5388c987b2b209bbfa461bca2bf1e09236863f8d
Fredrik Forsell <fredrik.forsell@outlook.com>
2019-05-25 04:53:45 +0200

    + Genre search and create

---
+++ MovieHunter.RESTApi/Controllers/GenresController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieHunter.DataAccessCore.Models;
+
+namespace MovieHunter.RESTApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly fredrifoContext _context;
+
+        public GenresController(fredrifoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Genres
+        [HttpGet]
+        public IEnumerable<Genre> GetGenre()
+        {
+            return _context.Genre;
+        }
+
+        // GET: api/Genres/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGenre([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var genre = await _context.Genre.FindAsync(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(genre);
+        }
+
+        // PUT: api/Genres/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenre([FromRoute] int id, [FromBody] Genre genre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != genre.GenreId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(genre).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Genres
+        [HttpPost]
+        [Route("new")]
+        public async Task<IActionResult> PostGenre([FromBody] Genre genre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Genre.Add(genre);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGenre", new { id = genre.GenreId }, genre);
+        }
+
+        // POST: api/Genres/search
+        [HttpPost]
+        [Route("search")]
+        public async Task<IActionResult> PostSearchGenre([FromBody] string currentSearch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var list = _context.Genre.Where(c => EF.Functions.Like(c.GenreName, currentSearch + "%"));
+            return Ok(list);
+        }
+
+        // DELETE: api/Genres/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenre([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var genre = await _context.Genre.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            _context.Genre.Remove(genre);
+            await _context.SaveChangesAsync();
+
+            return Ok(genre);
+        }
+
+        private bool GenreExists(int id)
+        {
+            return _context.Genre.Any(e => e.GenreId == id);
+        }
+    }
+}
